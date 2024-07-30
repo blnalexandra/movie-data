1bcb39a0fed6f7998eb7e0b91e9fa77e91ecfee2
blnalexandra <alexandra_bln@yahoo.com>
2024-07-30 22:07:53 +0300

    Delete filmapp/main.py

--- filmapp/main.py
+++
@@ -1,9 +0,0 @@
-
-from filmapp.repository.Repository import Repository
-from filmapp.service.Service import Service
-from filmapp.ui.UI import UI
-
-repository = Repository()
-service = Service(repository)
-ui = UI(service)
-ui.program()
